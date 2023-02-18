2694bac32fa9c2ad338e26073081ede317d0b2e6
Shawn Bragdon <shawnjb.contact@gmail.com>
2023-02-18 15:22:30 -0800

    Create repository.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Powershell Scripts
+
+## Description
+
+This repository contains a collection of Powershell scripts.
+
+## How to use
+
+1. Clone the repository
+2. Open the `powershell-scripts` folder
+3. Execute the script of your choice.
---
+++ scripts/Win-ContextMenu.ps1
@@ -0,0 +1,14 @@
+$regPath = 'HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}'
+$regItem = 'InprocServer32'
+
+if ( !(Test-Path $regPath\$regItem) )
+{
+    New-Item -Path "$regPath\\" -Name $regItem -Force | Out-Null
+    Set-ItemProperty -Path "$regPath\$regItem" -Name '(Default)' -Value $null -Type String -Force | Out-Null
+    taskkill.exe /f /im explorer.exe
+    Start-Process explorer.exe
+} else {
+    Remove-Item -Path "$regPath\$regItem" -Force | Out-Null
+    taskkill.exe /f /im explorer.exe
+    Start-Process explorer.exe
+}
